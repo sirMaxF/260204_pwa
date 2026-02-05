bb96154597e7a076ff0d4634eafc2a646d284c7d
sirMaxF <sirmaxfrei@mail.ru>
2026-02-05 17:06:26 +0300

    pwa-8

--- src/PWA/sw.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-importScripts('sw-toolbox.js');
-toolbox.precache(["index.html", "css/style.css", "js/app.min.js"]);
-toolbox.router.get('/img/*', toolbox.cacheFirst);
-toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5 });
